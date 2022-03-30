6c3388d519a96167961b91c616f4c582b5c56b0c
rachelakimana <rachelakimana@gmail.com>
2022-03-30 18:42:59 +0200

    add data

--- src/login.js
+++ src/login.js
@@ -20,7 +20,7 @@ loginbutn.addEventListener("click", (e) => {
       if (data.token) {
         window.location.href = "/My-brand/pages/dashboard.html";
       } else {
-        alert(data.message);
+        alert(data.Message);
         window.location.href = "/My-brand/pages/login.html";
       }
     });
